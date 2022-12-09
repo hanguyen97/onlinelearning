f13d4e79991282dbb0c56d4abb90874ec07e8cd9
Ha Nguyen <hanguye97@gmail.com>
2022-12-09 00:55:33 -0500

    add adagrad

--- R/functions.R
+++ R/functions.R
@@ -90,6 +90,61 @@ run_OGD <- function(reg, y, X, b_true, learn_rate, PR_ave=TRUE) {
 }
 
 
+#' Run adaptive gradient descent
+#'
+#' @param reg sample data from either `reg="linear"` or `reg = "logistic"`
+#' @param y response vector
+#' @param X signal data
+#' @param b_true true coefficients
+#' @param learn_rate learning rate
+#' @param PR_ave logical; for Polyak-Ruppert averaging
+#'
+#' @return
+#'* `y` response vector
+#'* `X` signal data
+#'* `b` coef vector
+#' @export
+run_AdaGrad <- function(reg, y, X, b_true, learn_rate, PR_ave=TRUE) {
+
+  b_len = length(b_true)
+  n_iter = nrow(X)
+
+  # initialize beta
+  b_est = numeric(b_len)
+  b_est_matrix = matrix(0, nrow = b_len, ncol = n_iter)
+  est_error = numeric(n_iter)
+  pred_error = numeric(n_iter)
+  G = numeric(b_len)
+
+  # loop through the most recent data point
+  for (i in 1:n_iter) {
+
+    # Update the gradient
+    gradient = get_gradient(x=X[i,], y[i], b=b_est, reg=reg)
+    G = G + gradient^2
+
+    # Update beta
+    b_est_raw = b_est - learn_rate * gradient /sqrt(G)
+    if (PR_ave==TRUE) {
+      # Polyak-Ruppert Averaging
+      b_est = (b_est_raw + b_est*(i-1))/i
+    } else {
+      b_est = b_est_raw
+    }
+    # Store new beta
+    b_est_matrix[, i] = b_est
+
+    # Calculate estimation and prediction errors
+    est_error[i] = est_error(b_true=b_true, b_est=b_est)
+    pred_error[i] = pred_error(reg=reg, y=y, X=X, b_est=b_est)
+  }
+
+  return(list(b_est = b_est,
+              b_est_past = b_est_matrix,
+              b_true = b_true,
+              est_error = est_error,
+              pred_error = pred_error))
+}
 
 
 
@@ -112,5 +167,20 @@ get_gradient = function(x, y, b, reg){
   if (reg == "logistic") {
     y = 2*y-1
     return(-y*x/ ( 1+ exp(y*sum(x*b) ) ) )
+  }
+}
+
+#2 --------- Calculate estimation and prediction errors ---------#
+est_error = function(b_true, b_est){
+  return(norm(b_true - b_est, type = "2"))
+}
+
+pred_error = function(reg, y, X, b_est) {
+  if (reg=="linear") {
+    # mean squared prediction error
+    return(sum((y - X %*% b_est)^2)/nrow(X))
+  } else if (reg=="logistic") {
+    # classification error rate
+    return(colMeans(sign(1/(1 + exp(-X%*%b_est))-0.5) != y))
   }
 }
